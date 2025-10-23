2e7047e919ed6fdfe9a601b976fcd09077ee92cb
Jeff Hornsberger <jhornsberger@gmail.com>
2025-10-23 09:33:14 -0700

    nvim: fix true color in tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -35,6 +35,7 @@ vim.o.lazyredraw = true
 vim.o.path = '**'
 vim.o.numberwidth = 1
 vim.o.previewheight = 20
+vim.o.termguicolors = true
 vim.o.winblend = 15
 vim.o.pumblend = 15
 vim.o.splitright = true
